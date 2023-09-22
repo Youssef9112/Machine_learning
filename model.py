306705ae8753fb8e73dd537aecc1c5549f6dcd6c
Youssef9112 <youssefshahean0@gmail.com>
2023-09-22 20:48:10 +0300

    created a new file where all the code exist. model.py contains only the architecture of the network

--- model.py
+++ model.py
@@ -1,12 +1,13 @@
 import numpy as np
 import h5py
+from helpers import gradient_checking, model
 
 # array that has L elements which represents the number of units in each layer including the input layer
 
-layers_dims = [12288, 128, 20, 7, 5, 1]
-activations = ["sigmoid", "relu", "relu", "relu", "relu", "sigmoid"]
+layers_dims = [12288, 20, 7, 5, 1]
+activations = ["no activation", "relu", "relu", "relu", "sigmoid"]
 L = len(layers_dims) - 1
-epsilon = 1e-8
+# np.random.seed(1)
 
 
 def main():
@@ -23,149 +24,11 @@ def main():
     train_x = train_x_flatten / 255.0
     test_x = test_x_flatten / 255.0
 
-    parameters, costs = model(train_x, train_y, activations, 1e-2, 1000)
-
-    # parameters, _ = network_initialization(layers_dims)
-    # print(parameters)
-    # Al, __ = network_forward_prop(train_x, parameters, activations)
-    # print(Al)
-
-    Al = predict(test_x, test_y, parameters, activations)
-    _ = predict(train_x, train_y, parameters, activations)
-
-
-def sigmoid(z):
-    z = np.clip(z, -500, 500)
-    return 1 / (1 + np.exp(-z))
-
-
-def relu(z):
-    return np.maximum(0, z)
-
-
-def der_sigmoid(x):
-    return sigmoid(x) * (1 - sigmoid(x))
-
-
-def der_relu(x):
-    return (x > 0).astype(int)
-
-
-def single_layer_initialization(units, n_x):
-    Wi = np.random.randn(units, n_x) * 0.01
-    bi = np.zeros((units, 1))
-    return Wi, bi
-
-
-def network_initialization(layers_dims: list):
-    L = len(layers_dims) - 1
-    parameters = {}
-    grads = {}
-    for l in range(L):
-        (
-            parameters["W" + str(l + 1)],
-            parameters["b" + str(l + 1)],
-        ) = single_layer_initialization(layers_dims[l + 1], layers_dims[l])
-        grads["dW" + str(l + 1)
-              ] = np.zeros((layers_dims[l + 1], layers_dims[l]))
-        grads["db" + str(l + 1)] = np.zeros((layers_dims[l + 1], 1))
-    return parameters, grads
-
-
-def single_layer_forward_prop(Ai, Wi, bi, activation):
-    Zi = np.dot(Wi, Ai) + bi  # shape = (no_of_units,m)
-    if activation == "relu":
-        Ai = relu(Zi)
-    elif activation == "sigmoid":
-        Ai = sigmoid(Zi)
-    return Ai, Zi
-
-
-def network_forward_prop(X: np.ndarray, parameters: dict, activation: list):
-    A1, Z1 = single_layer_forward_prop(
-        X, parameters["W1"], parameters["b1"], activation[1]
+    parameters, costs, params_list, grads_list = model(
+        train_x, train_y, activations, 1e-2, 2500
     )
-    A_layer = A1
-    cache = {}
-    cache["Z1"] = Z1
-    cache["A1"] = A1
-    cache["A0"] = X
-    for l in range(2, L + 1):
-        A_layer, Z_layer = single_layer_forward_prop(
-            A_layer,
-            parameters["W" + str(l)],
-            parameters["b" + str(l)],
-            activation[l],
-        )
-        cache["A" + str(l)] = A_layer
-        cache["Z" + str(l)] = Z_layer
-    AL = A_layer
-    return AL, cache
-
-
-def compute_cost(Y: np.ndarray, AL: np.ndarray, type: str):
-    if type == "cross entropy":
-        cost = -1 * ((Y * np.log(np.add(np.abs(AL), epsilon))) +
-                     (1 - Y) * (np.log(np.add(np.abs(1 - AL), epsilon)))).mean()
-        cost = np.squeeze(cost)
-    return cost
-
-
-def back_prop(
-    AL: np.ndarray,
-    Y: np.ndarray,
-    parameters: dict,
-    grads: dict,
-    cache: dict,
-    activations: list,
-):
-    dZ = AL - Y
-    m = Y.shape[1]
-    for l in range(L, 0, -1):
-        grads["dW" + str(l)] = np.dot(dZ, (cache["A" + str(l - 1)]).T) / m
-        grads["db" + str(l)] = np.sum(dZ, axis=1, keepdims=True) / m
-        if l == 1:
-            return grads
-        dZ = np.dot(parameters["W" + str(l)].T, dZ)
-        if activations[l - 1] == "sigmoid":
-            dZ = dZ * der_sigmoid(cache["Z" + str(l - 1)])
-        else:
-            dZ = dZ * der_relu(cache["Z" + str(l - 1)])
-    return grads
-
-
-def update_parameters(parameters, learning_rate, grads):
-    for l in range(L):
-        parameters["W" + str(l + 1)] = (
-            parameters["W" + str(l + 1)] - learning_rate *
-            grads["dW" + str(l + 1)]
-        )
-        parameters["b" + str(l + 1)] = (
-            parameters["b" + str(l + 1)] - learning_rate *
-            grads["db" + str(l + 1)]
-        )
-        return parameters
-
-
-def model(X, Y, activations, learning_rate, num_iterations):
-    costs = []
-    parameters, grads = network_initialization(layers_dims)
-    for i in range(num_iterations):
-        AL, cache = network_forward_prop(X, parameters, activations)
-        cost = compute_cost(Y, AL, "cross entropy")
-        grads = back_prop(AL, Y, parameters, grads, cache, activations)
-        parameters = update_parameters(parameters, learning_rate, grads)
-        if (i % 100 == 0) or (i == num_iterations):
-            costs.append(cost)
-            print(f"Iteration: {i} cost: {cost}")
-    return parameters, costs
-
-
-def predict(X: np.ndarray, Y: np.ndarray, parameters: dict, activations: list):
-    AL, _ = network_forward_prop(X, parameters, activations)
-    AL = (AL >= 0.5)
-    print("Accuracy: " + str(np.sum((AL == Y) / Y.shape[1])))
-    return AL
+    # Al = predict(test_x, test_y, parameters, activations)
+    # _ = predict(train_x, train_y, parameters, activations)
 
 
 def load_data(train_path, test_path):
@@ -180,8 +43,7 @@ def load_data(train_path, test_path):
     test_dataset = h5py.File(test_path, "r")
     # your test set features
     test_set_x_orig = np.array(test_dataset["test_set_x"][:])
-    test_set_y_orig = np.array(
-        test_dataset["test_set_y"][:])  # your test set labels
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:])  # your test set labels
 
     classes = np.array(test_dataset["list_classes"][:])  # the list of classes
 
--- test_file.py
+++
@@ -1,5 +0,0 @@
-print("youssef")
-
-
-print("hello,world")
-print("test")
